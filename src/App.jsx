91bdc264bf443c0b8ec5824071e7c853fd01233c
Mustaph John <mustcean@gmail.com>
2022-01-18 14:09:07 +0300

    game logic implemented

--- src/App.jsx
+++ src/App.jsx
@@ -37,41 +37,84 @@ function FormTodo({ addTodo }) {
   );
 }
 
-function App() {
-  const [score, setScore] = React.useState(0);
-  const [todos, setTodos] = React.useState([]);
 
-  const lastWord = todos[0]
 
-  const lenLastWord = lastWord!==undefined? lastWord.length:0
+function calcBalance(prev, curr){
+  if (prev.length == 0) return 1
+  else if(prev.length != 0 && Math.abs(prev.length-curr.length) > 1) return 0
+  else {
 
-  const addTodo = text => {
-    if(!todos.includes(text)){
-      const newTodos = [text.replace(/\s/g, ''), ...todos ];
-      setTodos(newTodos);
-      const isEqLen=text.length===lenLastWord
-      setScore(
-        score+ 
-          (
-            isEqLen?1:-1
-          )
-        )
+    if(prev.length == curr.length){
+      let diff = 0
+      for(let i = 0; i < prev.length; i++){
+        if(prev[i]!=curr[i]) diff++
+        if(diff>1)return 0
+      }
+
+    return 1
     }
-  };
+    else{
+      const shortW = prev.length> curr.length? curr:prev
+      const longW = prev.length< curr.length?curr:prev
+      for(let i = 0; i < longW.length; i++){
+          let trimmedLong = []
+          for(let j = 0; j < longW.length; j++) 
+            if(i != j) trimmedLong.push(longW[j])
+          if(trimmedLong.join("") == shortW) return -1
+      }
+      return 0
+    }
+
+  }
+}
+
+
+function calcScore(todos){
+  let score = 0
+    for (let i=todos.length-1; i > 0; i--){
+      const cb = calcBalance(todos[i-1], todos[i])
+      console.log(cb)
+      score += cb
+    }
+  return score
+}
+
+
+function App() {
+  
+  const [todos, setTodos] = React.useState([])
+
+  const [lastWord, setLastWord] = React.useState([])
+
+
+  React.useEffect(()=>{
+    setTodos([lastWord,...todos])
+  },[lastWord]);
+
 
   
+
+
+  const addTodo = _word => {
+    if(!_word.includes(' ') && !todos.includes(_word.toLowerCase())){
+      const cbal = calcBalance(lastWord, _word)
+      //console.log(cbal)
+      if(cbal == 1 || cbal == -1){
+        setLastWord(_word.toLowerCase())
+      }
+    }
+  };
 
   return (
     <div className="app">
       <div className="container">
         <h1 className="text-center mb-4">Worddapp</h1>
-        <a> Score: {score}</a><br/>
+        <a> Score: {calcScore(todos)}</a><br/>
         <br/>
         <FormTodo addTodo={addTodo} />
+
         <a> Last Word:</a><br/>
-        
-        <a>{lastWord} (length: {lastWord!==undefined? lastWord.length:''}) </a>
-        
+        <a>{lastWord} ({lastWord.length}) </a> 
         <br/>
         <br/>
          <a>History:</a>
@@ -88,6 +131,7 @@ function App() {
             </Card>
           ))}
         </div>
+
       </div>
     </div>
   );
